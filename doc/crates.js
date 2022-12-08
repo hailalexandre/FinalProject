e4536977c5b4a499ed6d3b16a8b3c4ffcdaa720d
hailalexandre <78934764+hailalexandre@users.noreply.github.com>
2022-12-07 22:45:03 -0500

    Add files via upload

---
+++ doc/crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["hangman_demo","libc","rand"];
---
+++ doc/source-files.js
@@ -0,0 +1,5 @@
+var sourcesIndex = {};
+sourcesIndex["hangman_demo"] = {"name":"","files":["main.rs"]};
+sourcesIndex["libc"] = {"name":"","files":["dox.rs","lib.rs","macros.rs","windows.rs"]};
+sourcesIndex["rand"] = {"name":"","dirs":[{"name":"distributions","files":["exponential.rs","gamma.rs","mod.rs","normal.rs","range.rs","ziggurat_tables.rs"]}],"files":["chacha.rs","isaac.rs","lib.rs","os.rs","rand_impls.rs","read.rs","reseeding.rs"]};
+createSourceSidebar();
